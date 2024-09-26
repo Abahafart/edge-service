b7321f68f470b78400bff1dc70f2133e424c68c9
Alfredo Miranda <alfredo.miranda.learning@gmail.com>
2024-09-25 18:21:27 -0600

    test containers with redis

--- src/test/java/com/arch/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/arch/edgeservice/EdgeServiceApplicationTests.java
@@ -2,9 +2,29 @@ package com.arch.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class EdgeServiceApplicationTests {
+
+  private static final int REDIS_PORT = 6379;
+
+  @Container
+  static GenericContainer<?> redisContainer = new GenericContainer<>("redis:latest").withExposedPorts(REDIS_PORT);
+
+  @DynamicPropertySource
+  static void redisProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.redis.port", redisContainer::getFirstMappedPort);
+    registry.add("spring.data.redis.host", redisContainer::getHost);
+  }
 
   @Test
   void contextLoads() {
